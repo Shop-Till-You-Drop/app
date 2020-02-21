458601ad123764d7ff6af810c4e7c9567f9d2099
Joe Lockwood <jtlockwo@mtu.edu>
2020-02-21 17:14:44 -0500

    Got email to be but in database format is user@gmail,com

--- app/src/main/java/com/example/shoptilyoudrop/MainActivity.java
+++ app/src/main/java/com/example/shoptilyoudrop/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,10 +30,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText food;
     private Button addName;
     private ListView listView;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, savedInstanceState);
         setContentView(R.layout.activity_main);
         logout = findViewById(R.id.logout);
         price = findViewById(R.id.price);
@@ -39,63 +44,66 @@ public class MainActivity extends AppCompatActivity {
         addName = findViewById(R.id.add);
         listView = findViewById(R.id.listView);
         Intent intent = getIntent();
-        final String fName = intent.getStringExtra("Test");
+        String tempName = "";
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this, "Signed Out", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this , StartActivity.class));
-            }
-        });
-        addName.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                String foodToText = food.getText().toString();
-                String priceToText = price.getText().toString();
-                if(foodToText.isEmpty()){
-                    Toast.makeText(MainActivity.this, "Invalid Food", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    if (fName != null && !fName.isEmpty()){
-                            String[] emailExt = fName.split("@");
+        if (intent.getStringExtra("Test") != null) {
+            tempName = intent.getStringExtra("Test");
+        }
+        if (tempName != null) {
+            final String fName = tempName.replace(".", ",");
 
-                            FirebaseDatabase database = FirebaseDatabase.getInstance();
-                            DatabaseReference mRef = database.getReference("").child("Database");
-                            mRef.child(emailExt[0]).child(foodToText).setValue(Double.parseDouble(priceToText));
-                            Toast.makeText(MainActivity.this, "Added to "+fName, Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Invalid Account", Toast.LENGTH_SHORT).show();
-                        }
-                }
-            }
-        });
-
-        final ArrayList<String> list = new ArrayList<>();
-        final ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
-        listView.setAdapter(adapter);
-        if (fName != null && !fName.isEmpty()) {
-            String[] emailExt = fName.split("@");
-            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Database");
-            reference.addValueEventListener(new ValueEventListener() {
+            logout.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    list.clear();
-                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
-                        if(snapshot.getValue()!=null) {
-                            list.add(snapshot.getValue().toString());
-                        }
-                    }
-                    adapter.notifyDataSetChanged();
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                public void onClick(View v) {
+                    FirebaseAuth.getInstance().signOut();
+                    Toast.makeText(MainActivity.this, "Signed Out", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, StartActivity.class));
                 }
             });
+            addName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String foodToText = food.getText().toString();
+                    String priceToText = price.getText().toString();
+                    if (foodToText.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Invalid Food", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (!fName.isEmpty()) {
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference mRef = database.getReference("").child("Database");
+                            mRef.child(fName).child(foodToText).setValue(Double.parseDouble(priceToText));
+                            Toast.makeText(MainActivity.this, "Added to " + fName, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Invalid Account", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+
+            final ArrayList<String> list = new ArrayList<>();
+            final ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+            listView.setAdapter(adapter);
+            if (!fName.isEmpty()) {
+//            String[] temp = fName.split("@");
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Database").child(fName);
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        list.clear();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            if (snapshot.getValue() != null) {
+                                list.add(snapshot.getKey() + ": " + snapshot.getValue().toString());
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
         }
     }
 }
