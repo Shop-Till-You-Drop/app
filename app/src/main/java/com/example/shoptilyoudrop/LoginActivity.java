cf78aa78c2649f9daa5e79d16816964323d52eac
Joe Lockwood <jtlockwo@mtu.edu>
2020-03-02 13:36:23 -0500

    Upgrade gradle and hides entire password

--- app/src/main/java/com/example/shoptilyoudrop/LoginActivity.java
+++ app/src/main/java/com/example/shoptilyoudrop/LoginActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.method.TransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -28,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ((TextView) findViewById(R.id.password)).setTransformationMethod(new HiddenPassTransformationMethod());
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
@@ -96,5 +99,41 @@ public class LoginActivity extends AppCompatActivity {
 
     public FirebaseAuth getAuthor() {
         return author;
+    }
+
+    //Function that hides the character code based off of https://stackoverflow.com/questions/6360222/problem-with-android-password-field-not-hiding-the-last-character-typed/16202479
+    private class HiddenPassTransformationMethod implements TransformationMethod {
+        private char passwordHider = '\u2022';
+        //Passes chars
+        @Override
+        public CharSequence getTransformation(final CharSequence charSequence, final View view) {
+            return new PassCharSequence(charSequence);
+        }
+        //We are not going to be implementing this function
+        @Override
+        public void onFocusChanged(final View view, final CharSequence charSequence, final boolean b, final int i, final Rect rect) {
+        }
+
+        private class PassCharSequence implements CharSequence {
+            private final CharSequence charSequence;
+            public PassCharSequence(final CharSequence charSequence) {
+                this.charSequence = charSequence;
+            }
+            //Get the char at every location
+            @Override
+            public char charAt(final int index) {
+                return passwordHider;
+            }
+            //Get length of current password
+            @Override
+            public int length() {
+                return charSequence.length();
+            }
+            //Hides all the characters
+            @Override
+            public CharSequence subSequence(final int start, final int end) {
+                return new PassCharSequence(charSequence.subSequence(start, end));
+            }
+        }
     }
 }
