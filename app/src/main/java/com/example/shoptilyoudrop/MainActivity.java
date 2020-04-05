7e5f2dee68a419097e81655c81c81132d5e4089e
Holly Johnson <hollyj@mtu.edu>
2020-04-05 18:05:03 -0400

    Working on Google-Sign in. Fixed some more errors. Moved code from MainActivity to StartActivity.

--- app/src/main/java/com/example/shoptilyoudrop/MainActivity.java
+++ app/src/main/java/com/example/shoptilyoudrop/MainActivity.java
@@ -46,11 +46,6 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter adapter;
     private Button checkout;
     private String fName;
-    private SignInButton signInButton;
-    private GoogleSignInClient mGoogleSignInClient;
-    private String TAG = "MainActivity";
-    private FirebaseAuth mAuth;
-    private int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,23 +59,6 @@ public class MainActivity extends AppCompatActivity {
         food = findViewById(R.id.food);
         addName = findViewById(R.id.add);
         checkout = findViewById(R.id.checkOut);
-
-        signInButton = findViewById(R.id.sign_in_button);
-        mAuth = FirebaseAuth.getInstance();
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-
-
 
         Intent intent = getIntent();
         String tempName = "";
@@ -149,68 +127,7 @@ public class MainActivity extends AppCompatActivity {
         food.setAdapter(adapter);
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try{
-            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
-            Toast.makeText(MainActivity.this, "Signed In Successfully", Toast.LENGTH_SHORT).show();
-            FirebaseGoogleAuth(acc);
-        }
-        catch (ApiException e) {
-
-            Toast.makeText(MainActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
-            FirebaseGoogleAuth(null);
-        }
-
-    }
-
-    private void FirebaseGoogleAuth(GoogleSignInAccount acct) {
-        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                    updateUI(null);
-                }
-
-            }
-        });
-    }
-
-    private void updateUI(FirebaseUser fUser){
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if(account !=  null){
-            String personName = account.getDisplayName();
-            String personGivenName = account.getGivenName();
-            String personFamilyName = account.getFamilyName();
-            String personEmail = account.getEmail();
-            String personId = account.getId();
-            Uri personPhoto = account.getPhotoUrl();
-
-            Toast.makeText(MainActivity.this,personName + personEmail ,Toast.LENGTH_SHORT).show();
-        }
-
-    }
 
     public AutoCompleteTextView getFood() {
         return food;
