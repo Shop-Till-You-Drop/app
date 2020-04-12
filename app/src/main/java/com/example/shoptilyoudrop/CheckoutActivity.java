2ae18447f6b0fc71c8ce8c70eff5eda9cc7fe5d8
tuckniels <43764095+tuckniels@users.noreply.github.com>
2020-04-12 15:04:47 -0400

    Icon finished

--- app/src/main/java/com/example/shoptilyoudrop/CheckoutActivity.java
+++ app/src/main/java/com/example/shoptilyoudrop/CheckoutActivity.java
@@ -1,16 +1,10 @@
 package com.example.shoptilyoudrop;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextClock;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
