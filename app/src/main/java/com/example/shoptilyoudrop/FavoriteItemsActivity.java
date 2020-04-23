719b479e909114b8ea00fc8cea023050d8717055
Fenrious <sfkrogel@charter.net>
2020-04-23 19:50:51 -0400

    Progress on FavoriteItemsActivity. Lists all favorite food/store combinations listed under a food item. Remove from Favorites works per food/store combination. Will list nothing if no food items contain the "Favorite" child.

--- app/src/main/java/com/example/shoptilyoudrop/FavoriteItemsActivity.java
+++ app/src/main/java/com/example/shoptilyoudrop/FavoriteItemsActivity.java
@@ -1,6 +1,9 @@
 package com.example.shoptilyoudrop;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,9 +15,19 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,64 +35,121 @@ import java.util.List;
 
 public class FavoriteItemsActivity extends AppCompatActivity {
     private ArrayList<String> favorites = new ArrayList<>();
+    private ArrayList<String> stores = new ArrayList<>();
+    private ArrayList<String> prices = new ArrayList<>();
+    private String userName;
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_items);
-        ListView listView = (ListView) findViewById(R.id.listview);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        userName = getIntent().getStringExtra("Test");
         generateListContent();
-        listView.setAdapter((new MyListAdapter (this, R.layout.list_item, favorites)));
     }
 
     private void generateListContent() {
-        for(int i = 0; i < 10; i++) {
-            favorites.add("Pineapple");
-        }
+        DatabaseReference ref = firebaseDatabase.getReference("").child("Database").child(userName);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                favorites = new ArrayList<>();
+                stores = new ArrayList<>();
+                prices = new ArrayList<>();
+                for (DataSnapshot snapshotfood : dataSnapshot.getChildren()) {
+                    if(snapshotfood.child("Favorite").getChildrenCount() > 0) {
+                        favorites.add(snapshotfood.getKey());
+                        for (DataSnapshot snapshotstore : snapshotfood.child("Favorite").getChildren()) {
+                            if(favorites.size() == stores.size()) {
+                                favorites.add(favorites.get(favorites.size() - 1));
+                            }
+                            stores.add(snapshotstore.getKey());
+                            if(snapshotstore.getValue() instanceof Double) {
+                                prices.add(Double.toString((Double) snapshotstore.getValue()));
+                            } else if (snapshotstore.getValue() instanceof Long) {
+                                prices.add(Long.toString((Long) snapshotstore.getValue()));
+                            }
+                            System.out.println(favorites);
+                        }
+                    }
+                }
+                if(favorites.isEmpty()) {
+                    Toast.makeText(FavoriteItemsActivity.this, "No favorites found on profile.", Toast.LENGTH_LONG).show();
+                }
+                favorites.add("");
+                stores.add("");
+                prices.add("");
+                ListView listView = findViewById(R.id.listview);
+                listView.setAdapter((new MyListAdapter (FavoriteItemsActivity.this, favorites, stores, prices)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(FavoriteItemsActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private class MyListAdapter extends ArrayAdapter<String> {
-        private int layout;
-        private MyListAdapter(Context context, int resource, List<String> objects) {
-            super(context, resource, objects);
-            layout = resource;
+        private ArrayList<String> foodType;
+        private ArrayList<String> stores;
+        private ArrayList<String> prices;
+
+        private MyListAdapter(Context context, ArrayList<String> foodType, ArrayList<String> store, ArrayList<String> price) {
+            super(context, R.layout.list_item, R.id.list_item_name, foodType);
+            this.foodType = foodType;
+            this.stores = store;
+            this.prices = price;
         }
 
+        @NonNull
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder mainViewHolder = null;
-            if(convertView == null) {
-                LayoutInflater inflator = LayoutInflater.from(getContext());
-                convertView = inflator.inflate(layout, parent, false);
-                ViewHolder viewHolder = new ViewHolder();
-                viewHolder.name = (TextView) convertView.findViewById(R.id.list_item_name);
-                viewHolder.store = (TextView) convertView.findViewById(R.id.list_item_store);
-                viewHolder.price = (TextView) convertView.findViewById(R.id.list_item_price);
-                viewHolder.addToCheckout = (Button) convertView.findViewById(R.id.list_item_add_to_checkout);
-                viewHolder.addToCheckout.setOnClickListener(new View.OnClickListener() {
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = inflater.inflate(R.layout.list_item, parent, false);
+            TextView name = row.findViewById(R.id.list_item_name);
+            final TextView store = row.findViewById(R.id.list_item_store);
+            TextView price = row.findViewById(R.id.list_item_price);
+            Button addToCheckout = row.findViewById(R.id.list_item_add_to_checkout);
+            Button removeFromFavorites = row.findViewById(R.id.list_item_remove_from_favorites);
+
+            if(position == favorites.size() - 1) {
+                addToCheckout.setText(R.string.back);
+                removeFromFavorites.setText("");
+                addToCheckout.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(FavoriteItemsActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(FavoriteItemsActivity.this, MenuActivity.class);
+                        i.putExtra("Test", userName);
+                        startActivity(i);
+                        finish();
                     }
                 });
-                viewHolder.removeFromFavorites = (Button) convertView.findViewById(R.id.list_item_remove_from_favorites);
-                convertView.setTag(viewHolder);
+                removeFromFavorites.setVisibility(View.GONE);
             } else {
-                mainViewHolder = (ViewHolder) convertView.getTag();
-                mainViewHolder.name.setText(getItem(position));
+                addToCheckout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(FavoriteItemsActivity.this, favorites.get(position) + " added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                removeFromFavorites.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatabaseReference refRemove = firebaseDatabase.getReference().child("").child("Database").child(userName);
+                        refRemove = refRemove.child(favorites.get(position)).child("Favorite").child(stores.get(position));
+                        refRemove.removeValue();
+                        Toast.makeText(FavoriteItemsActivity.this, favorites.get(position) + " removed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
-            return convertView;
+            name.setText(foodType.get(position));
+            store.setText(stores.get(position));
+            price.setText(prices.get(position));
+
+            return row;
         }
-
-    }
-
-    public class ViewHolder {
-
-        TextView name;
-        TextView store;
-        TextView price;
-        Button addToCheckout;
-        Button removeFromFavorites;
     }
 }
